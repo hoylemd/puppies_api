a9dcd9b663e839031d0f389de58e1081827b2483
Michael D. Hoyle <hoylemd@gmail.com>
2017-09-30 13:31:50 -0400

    finish filling out the Puppy model

--- puppies/models.py
+++ puppies/models.py
@@ -10,3 +10,5 @@ def user_directory_path(instance, filename):
 class Puppy(TimeStampedModel):
     owner = models.ForeignKey(get_user_model())
     file = models.FileField(upload_to=user_directory_path)  # TODO ImageField?
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField()
