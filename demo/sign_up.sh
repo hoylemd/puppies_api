8c5c5059c9796eaab890ab7b7800cec9ffc83c5b
Michael D. Hoyle <hoylemd@gmail.com>
2017-10-01 17:41:47 -0400

    add signup demo script

---
+++ demo/sign_up.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+HEADERS='Accept:application/json;'
+BASE_URL=http://127.0.0.1:8000
+
+USERNAME='imperatorsboy'
+PASSWORD='4julian'
+FIRST_NAME='Cassius'
+LAST_NAME='au+Belowna'
+EMAIL='cassius@aubelowna.com'
+
+DATA="username=$USERNAME&password=$PASSWORD&first_name=$FIRST_NAME"
+DATA="$DATA&last_name=$LAST_NAME&email=$EMAIL"
+
+command="curl -H \"$HEADERS\" -X POST -d $DATA $BASE_URL/register"
+
+echo ">>$command"
+echo
+
+echo $($command)
