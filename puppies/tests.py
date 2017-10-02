d5ff6cf2bc581791e83d78fa7b47ea0d62b48278
Michael D. Hoyle <hoylemd@gmail.com>
2017-10-02 14:26:00 -0400

    add some more tests

--- puppies/tests.py
+++ puppies/tests.py
@@ -1,4 +1,7 @@
+import json
+
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
 
@@ -98,3 +101,79 @@ class UserDetailsTest(TestCase):
         self.administrator.refresh_from_db()
         self.assertEqual(self.administrator.first_name, 'Fitchner')
         self.assertEqual(self.administrator.last_name, 'au Barca')
+
+
+class PuppyListTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.administrator = get_user_model().objects.create(
+            username='admin',
+            is_staff=True,
+            is_superuser=True,
+        )
+        self.administrator.set_password('secret123')
+        self.administrator.save()
+
+        self.user = get_user_model().objects.create(
+            username='reaper',
+            first_name='Darrow',
+            last_name='au Andromedus'
+        )
+        self.user.set_password('secret123')
+        self.user.save()
+
+        file_mock = SimpleUploadedFile('pupper.jpg', "pretend i'm a picture")
+        self.puppy_1 = self.user.puppy_set.create(
+            title='look! a puppy!',
+            body="Oh man I love puppies they're so cute and fluffy",
+            file=file_mock,
+        )
+        self.puppy_2 = self.administrator.puppy_set.create(
+            title='My puppy is the best',
+            file=file_mock,
+        )
+        self.puppy_3 = self.user.puppy_set.create(
+            body='Lorem ipsum and all that jazz.',
+            file=file_mock,
+        )
+
+    def abs_reverse(self, name, kwargs=None):
+        """As reverse(), but with testserver domain"""
+        return 'http://testserver{}'.format(reverse(name, kwargs=kwargs))
+
+    def test_get_puppy(self):
+        """Should return post details"""
+        response = self.client.get(
+            reverse('puppy-detail', kwargs={'pk': self.puppy_1.id}),
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        payload = json.loads(response.content)
+
+        self.assertEqual(
+            payload['owner'],
+            self.abs_reverse('user-detail', kwargs={'pk': self.user.pk}),
+        )
+
+    def test_get_feed(self):
+        """Should return all 3 posts in the correct order"""
+        response = self.client.get(
+            reverse('puppy-list')
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        payload = json.loads(response.content)
+
+        self.assertEqual(payload['count'], 3)
+        posts = payload['results']
+        self.assertEqual(
+            posts[0]['url'],
+            self.abs_reverse('puppy-detail', kwargs={'pk': self.puppy_3.pk}),
+        )
+        self.assertEqual(
+            posts[2]['url'],
+            self.abs_reverse('puppy-detail', kwargs={'pk': self.puppy_1.pk}),
+        )
