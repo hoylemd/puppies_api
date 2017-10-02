841463f8f5c22871770b6f7b05f6a5a232db9f0e
Michael D. Hoyle <hoylemd@gmail.com>
2017-10-01 21:46:02 -0400

    add demos for users' posts and user profile

---
+++ demo/users_posts.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+HEADERS='Accept:application/json;'
+BASE_URL=http://127.0.0.1:8000
+
+USERNAME='imperatorsboy'
+PASSWORD='4julian'
+
+command="curl -H \"$HEADERS\" -u $USERNAME:$PASSWORD $BASE_URL/users/2/posts/"
+
+echo ">>$command"
+echo
+
+echo $($command) | python -m json.tool
